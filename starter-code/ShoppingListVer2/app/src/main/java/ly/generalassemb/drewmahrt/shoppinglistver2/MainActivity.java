77142a3613364790255e84d711c1614d1a00a60f
KorbKenny <korbkenny@gmail.com>
2016-10-26 07:25:22 -0400

    finished

--- starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/MainActivity.java
+++ starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/MainActivity.java
@@ -2,13 +2,19 @@ package ly.generalassemb.drewmahrt.shoppinglistver2;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+
+import java.util.List;
 
 import ly.generalassemb.drewmahrt.shoppinglistver2.setup.DBAssetHelper;
 
@@ -21,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
         //Ignore the two lines below, they are for setup
         DBAssetHelper dbSetup = new DBAssetHelper(MainActivity.this);
         dbSetup.getReadableDatabase();
+
+        List<ItemObject> list = ReallyCoolSQLiteOpenHelper.getInstance(this).getAllAsList();
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(new CustomRecyclerViewAdapter(list));
 
     }
 }
